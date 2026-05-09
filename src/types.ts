0c8601f50985d5fc99b3b9a6927bec765b8fffd9
smrrvv1 <samira.b@compasscollege.art>
2026-05-09 17:59:52 +0600

    add types

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface IStartup {
+    id: number;
+    title: string;
+    one_liner: string;
+    description: string;
+    tags: string;
+    stage: string;
+    target_amount: string;
+    owner_id: number;
+}
