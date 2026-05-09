8a8ce86eaa41c792b13f86554fb8266038a02827
smrrvv1 <samira.b@compasscollege.art>
2026-05-09 18:13:48 +0600

    add homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
+import { axiosApi } from '../../axiosApi';
+import type { IStartup } from '../../types';
+import { StartupCard } from '../../components/StartupCard/StartupCard';
+import styles from './styles.module.css';
+
 export const HomePage = () => {
+  const [startups, setStartups] = useState<IStartup[]>([])
+
+  useEnhancedEffect(() => {
+    const getStartups = async() => {
+        try {
+            const response = await axiosApi.get('/startups/')
+
+            setStartups(response.data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    getStartups()
+    }, [])
+
     return (
-      <div>
-        <h1>Home Page</h1>
+      <div className={styles.container}>
+        {
+          startups.map((startup) => (
+            <StartupCard
+                key={startup.id}
+                startup={startup}
+            />
+          ))
+        }
       </div>
     )
   }
